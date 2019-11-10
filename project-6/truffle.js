22750e0e2a64feedecbe6cd3185f9b91ab7fafa6
afindl3 <alisonfindlay93@hotmail.com>
2019-11-10 14:13:07 -0500

    Project 6 requirements complete

--- project-6/truffle.js
+++ project-6/truffle.js
@@ -1,9 +1,22 @@
+// Check Term 1 Project 5 truffle-config.js for setup instructions
+const HDWalletProvider = require('truffle-hdwallet-provider');
+const infuraKey = "41f148ee4f404b88b6ebe25c0cfafbb7";
+
+const fs = require('fs');
+const mnemonic = fs.readFileSync(".secret").toString().trim();
+
 module.exports = {
   networks: {
     development: {
       host: "127.0.0.1",
       port: 8545,
       network_id: "*" // Match any network id
+    },
+    rinkeby: {
+      provider: () => new HDWalletProvider(mnemonic, `https://rinkeby.infura.io/v3/${infuraKey}`),
+      network_id: 4,       // rinkeby's id
+      gas: 4500000,        // rinkeby has a lower block limit than mainnet
+      gasPrice: 10000000000
     }
   }
 };
